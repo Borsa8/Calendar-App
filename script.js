e842a1e7043c25c50029915520f4e4792976308e
borsa <129904894+Borsa8@users.noreply.github.com>
2023-11-26 06:11:19 +0000

    timedive functions added

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ $(document).ready(function () {
   // Get the current day
   const currentHour = dayjs().hour();
 
- 
+// Clear content
+  $("#clearFieldsBtn").click(function (event) {
+    event.preventDefault;
+    $("textarea").val("");
+    localStorage.clear();
+  });
+
+  // Check each if it is in the past, present, or future
+  $(".time-div").each(function () {
+
+    // Split and convert to numerical values
+    var timeDiv = parseInt($(this).attr("id").split("-")[1]);
 });
