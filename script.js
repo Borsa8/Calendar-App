693d6060e93d47a6a6a42e2639e1c3e0f4b99b7d
borsa <129904894+Borsa8@users.noreply.github.com>
2023-11-26 06:34:22 +0000

    saveBtn stores in local storage

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ $(document).ready(function () {
   const today = dayjs().format("dddd, MMMM D, YYYY");
   currentDayElement.textContent = today;
 
-  // Get the current day
+// Get the current day
   const currentHour = dayjs().hour();
 
 // Clear content
-  $("#clearFieldsBtn").click(function (event) {
-    event.preventDefault;
-    $("textarea").val("");
-    localStorage.clear();
-  });
+$("#clearFieldsBtn").click(function (event) {
+  event.preventDefault;
+  $("textarea").val("");
+  localStorage.clear();
+});
 
 // Check each if it is in the past, present, or future
   $(".time-div").each(function () {
 
 // Split and convert to numerical values
-    var timeDiv = parseInt($(this).attr("id").split("-")[1]);
+  var timeDiv = parseInt($(this).attr("id").split("-")[1]);
 });
 
 // Compare the current time with the event time
@@ -35,9 +35,19 @@ if (currentHour == timeDiv) {
   $(this).addClass("future");
 } else if (currentHour > timeDiv) {
 
-  // If it is after, set past
+// If it is after, set past
   $(this).removeClass("future");
   $(this).addClass("past");
 }
-
 });
+
+// Save button saves in local storage
+
+$(".saveBtn").click(function (event) {
+  event.preventDefault();
+  var value = $(this).siblings(".time-block").val();
+  var time = $(this).parent().attr("id").split("-")[1];
+  localStorage.setItem(time, value);
+});
+
+
